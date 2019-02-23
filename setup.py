f5cd8b3988e27fde919aca51f7125dc429f95c44
Egor Dranischnikow <egor.dranischnikow@googlemail.com>
2019-02-23 23:36:37 +0100

    using prebuilt so-file

--- setup.py
+++ setup.py
@@ -1,14 +1,31 @@
 from setuptools import setup, find_packages, Extension
 
 
+#right path to shared-object:
+import os
+import sys
+from distutils.util import get_platform
+
+def get_path_to_sharedobject():
+    PLAT_SPEC = "{platform}-{version[0]}.{version[1]}".format(platform=get_platform(), version=sys.version_info)
+    return os.path.join("build", "lib." + PLAT_SPEC, 'commonso')
+
+
+# Cython-extensions:
 setter_ext = Extension(
                 name='commonso.setter',
-                sources = ["src/commonso/setter.pyx", "src/commonso/common.c"]
+                sources = ["src/commonso/setter.pyx"],
+                extra_link_args=["-Wl,-rpath=$ORIGIN/."],
+                libraries = ['common'],
+                library_dirs=[get_path_to_sharedobject()],
               )
 
 getter_ext = Extension(
                 name='commonso.getter',
-                sources = ["src/commonso/getter.pyx", "src/commonso/common.c"]
+                sources = ["src/commonso/getter.pyx"],
+                extra_link_args=["-Wl,-rpath=$ORIGIN/."],
+                libraries = ['common'],
+                library_dirs=[get_path_to_sharedobject()],
               )
 
 kwargs = {
@@ -18,6 +35,9 @@ kwargs = {
 
       'packages' : find_packages(where='src'),
       'package_dir' : {"": "src"},
+      'package_data' : { 'commonso': ['*.so']},
+      'include_package_data' : True,
+      'zip_safe' : False  #needed because setuptools are used
 }
 
 
